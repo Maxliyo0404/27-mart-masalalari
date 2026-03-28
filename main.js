ca51e45942c1db1013f6387ffa47129eabe3e602
Mahliyo <gasandigital13@gmail.com>
2026-03-28 22:55:40 +0500

    masalalar if else ichma ich kirish

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ let b = +prompt("b sonini kiriting");
 let c = +prompt("c sonini kiriting");
 let musbat = 0;
 let manfiy = 0;
-if () {
-    
-} else {
-    
+if (a > 0) {
+    alert("musbat ++")
+} else if ( a < 0) {
+     alert("manfiy ++")
 }
