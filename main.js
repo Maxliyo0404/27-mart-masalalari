6a2d6fe9bbec727ddd6b01215f7b2daf9c6a01ca
Mahliyo <gasandigital13@gmail.com>
2026-03-30 12:58:49 +0500

    8-masalani yakunladim

--- main.js
+++ main.js
@@ -212,6 +212,6 @@
 //  let kichik = Math.min(a, b);
 //  console.log(katta, kichik);
 // 8-masala
- let a = +prompt("a kiriting");
- let b = +prompt("b kiriting");
- let result = ( a !=== b) ? (a + b)
+//  let a = +prompt("a kiriting");
+//  let b = +prompt("b kiriting");
+//  let result = ( a !=== b) ? (a + b)
